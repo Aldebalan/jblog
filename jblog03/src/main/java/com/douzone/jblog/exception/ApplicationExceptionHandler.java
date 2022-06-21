4106ae5a4755692d5a1ff76eefdb883039ed90e3
Aldebalan <tjdgus4093@naver.com>
2022-06-21 18:23:03 +0900

    main up

--- jblog03/src/main/java/com/douzone/jblog/exception/ApplicationExceptionHandler.java
+++ jblog03/src/main/java/com/douzone/jblog/exception/ApplicationExceptionHandler.java
@@ -3,8 +3,8 @@ package com.douzone.jblog.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
